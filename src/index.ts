d8721283f43fc9ea4a709e42a871426ec4275f1c
Chris Case <ccase@gaig.com>
2024-03-07 17:17:53 -0600

    minor fixes.

--- src/index.ts
+++ src/index.ts
@@ -19,9 +19,47 @@ import { hasOwn } from './utils/objects';
 
 //----------------------------------------------------------------------------------------------------------------------
 
+interface DB2ConnectionParams
+{
+    CMT ?: number;
+    CONNTYPE ?: number;
+    DBQ ?: string;
+    MAXDECPREC ?: 31 | 63;
+    MAXDECSCALE ?: number;
+    MINDIVSCALE ?: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+    NAM ?: 0 | 1;
+    DFT ?: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
+    DSP ?: 0 | 1 | 2 | 3 | 4;
+    DEC ?: 0 | 1;
+    DECFLOATERROROPTION ?: 0 | 1;
+    DECFLOATROUNDMODE ?: 0 | 1 | 2 | 3 | 4 | 5 | 6;
+    MAPDECIMALFLOATDESCRIBE ?: 1 | 3;
+    ALLOWPROCCALLS : 0 | 1;
+}
+
+interface DB2ConnectionConfig
+{
+    database : string;
+    host : string;
+    port : number;
+    user : string;
+    password : string;
+    connectionStringParams ?: DB2ConnectionParams;
+}
+
+export interface DB2Config
+{
+    client : any;
+    connection : DB2ConnectionConfig;
+    pool ?: Knex.PoolConfig;
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
 class DB2Client extends knex.Client
 {
-    constructor(config)
+    constructor(config : Knex.Config<DB2Config>)
     {
         super(config);
         this.driverName = 'ibm_db';
@@ -273,50 +311,6 @@ class DB2Client extends knex.Client
                 return rows;
         }
     }
-}
-
-//----------------------------------------------------------------------------------------------------------------------
-
-interface DB2PoolConfig
-{
-    min ?: number;
-    max ?: number;
-    acquireConnectionTimeout ?: number;
-}
-
-interface DB2ConnectionParams
-{
-    CMT ?: number;
-    CONNTYPE ?: number;
-    DBQ ?: string;
-    MAXDECPREC ?: 31 | 63;
-    MAXDECSCALE ?: number;
-    MINDIVSCALE ?: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-    NAM ?: 0 | 1;
-    DFT ?: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
-    DSP ?: 0 | 1 | 2 | 3 | 4;
-    DEC ?: 0 | 1;
-    DECFLOATERROROPTION ?: 0 | 1;
-    DECFLOATROUNDMODE ?: 0 | 1 | 2 | 3 | 4 | 5 | 6;
-    MAPDECIMALFLOATDESCRIBE ?: 1 | 3;
-    ALLOWPROCCALLS : 0 | 1;
-}
-
-interface DB2ConnectionConfig
-{
-    database : string;
-    host : string;
-    port : 50000 | number;
-    user : string;
-    password : string;
-    connectionStringParams ?: DB2ConnectionParams;
-}
-
-export interface DB2Config
-{
-    client : any;
-    connection : DB2ConnectionConfig;
-    pool ?: DB2PoolConfig;
 }
 
 //----------------------------------------------------------------------------------------------------------------------
